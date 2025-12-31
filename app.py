aba8e0f32afc66fe397f2ee9434b247500e98610
sayedshoaib22 <shoaib3869224@gmail.com>
2025-12-31 21:59:22 +0530

    update

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ def send_mail():
     if not name or not email or not message:
         return jsonify(success=False, error='Missing required fields'), 400
 
-    GMAIL_USER = os.environ.get('vartisticstudio@gmail.com')
-    GMAIL_APP_PASSWORD = os.environ.get('mdaa ypie qgrl bkhl')
+    GMAIL_USER = os.environ.get('sayedshoaib3869224@gmail.com')
+    GMAIL_APP_PASSWORD = os.environ.get('stim lvpg robc tkar')
 
     if not GMAIL_USER or not GMAIL_APP_PASSWORD:
         return jsonify(success=False, error='Mail credentials not configured'), 500
